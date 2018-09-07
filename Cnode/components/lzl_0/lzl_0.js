b0932be76ceb0e5c4fdf640ddd707eca4e6e53fe
li-zheng-liang <2966833040@qq.com>
2018-09-07 18:51:40 +0800

    第三次

---
+++ Cnode/components/lzl_0/lzl_0.js
@@ -0,0 +1,18 @@
+Vue.component('herda',{
+    template:`
+    <div class="h-b">
+    <div class="h-b-di">
+        <img class="h-b-img" src="//o4j806krb.qnssl.com/public/images/cnodejs_light.svg" alt="">
+        <input class="h-b-di-in" type="text">
+    </div>
+    <div class="h-b-div">
+        <div>首页</div>
+        <div>新手入门</div>
+        <div>API</div>
+        <div>关于</div>
+        <div>注册</div>
+        <div>登录</div>
+    </div>
+</div>
+    `
+})
